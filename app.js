bf81014f654955ed1ce2413b883a15ec5fa1ce81
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-15 17:30:43 +0700

    Update app.js Rev00 First issued

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// write code here
+let menu = {
+  width: 200,
+  height: 300,
+  title: 'My menu'
+};
+
+function multiplyNumeric(obj, num) {
+for (let key in obj) {
+  if (!isNaN(obj[key]))
+  obj[key] = obj[key] * num
+}
+};
+console.log(`Initial WxH `,menu)
+
+
+multiplyNumeric(menu, 3)
+console.log(`After multiply `,menu)
+// menu = {
+//  width: 600,
+//  height: 900,
+//  title: 'My menu'
+// };
